fd4e438945c2aa6ee1189fe77bad41c7a2c2e592
snowy2002 <3099508313@qq.com>
2024-04-18 14:25:35 +0800

    123

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define time chrono::system_clock::now().time_since_epoch().count()
+mt19937_64 rnd(time);
+#define maxn 1000005
+#define int long long
+
+int read() {int x;cin>>x;return x;}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+}

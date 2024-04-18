15c58289225244c1348b5a3d957525fe8b99b99c
snowy2002 <3099508313@qq.com>
2024-04-18 14:31:18 +0800

    123

--- 1.cpp
+++ 1.cpp
@@ -7,8 +7,11 @@ mt19937_64 rnd(time);
 
 int read() {int x;cin>>x;return x;}
 
-signed main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+signed main() {
+    #ifdef ONLINE_JUDGE
+        ios::sync_with_stdio(false);
+        cin.tie(0);cout.tie(0);
+    #else
+        freopen("a.in", "r", stdin);
+    #endif
 }
